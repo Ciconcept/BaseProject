5d6afd175fffad37adb399f3d8a263addc78553b
Ciconcept <cikeng.concept@gmail.com>
2015-10-25 18:22:15 +0100

    Add Files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name baseProjectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the baseProjectApp
+ */
+angular.module('baseProjectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name baseProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the baseProjectApp
+ */
+angular.module('baseProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
